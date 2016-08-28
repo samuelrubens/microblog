1e3d41dc04bdc162beba5ae10150c35acbbc5487
Samuel Rubens <samuel.rubens@live.com>
2016-08-28 13:33:24 -0300

    Add hello action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # pagina incial para evitar conflitos no heroku com a pagina padrão do rails
+  def hello
+  	render html: "Olá, Seja bem vindo."
+  end
 end
