ec64a08be61143a1adc98be5c39c88a0d32c8014
Samuel Rubens <samuel.rubens@live.com>
2016-08-29 11:09:36 -0300

    Finalizando paginas estaticas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   # pagina incial para evitar conflitos no heroku com a pagina padrão do rails
-  def hello
-  	render html: "Olá, Seja bem vindo."
-  end
+
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,8 +2,13 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
    def setup
-    @base_title = "MicroMsg Ruby on Rails"
-   end 
+    @base_title = "Ruby on Rails MicroMsg"
+   end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
 
   test "should get home" do
     get static_pages_home_url
@@ -20,6 +25,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
   	get static_pages_about_url
   	assert_response :success
-    assert_select "title", "About | #{@base_title}"
+      assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
